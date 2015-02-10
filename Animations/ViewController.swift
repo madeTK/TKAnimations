6584ee08a2cf3268dd004cbae8fb9f6cdd801629
Marc Felden <marcfelden@fsmn0041.local>
2015-02-10 09:31:37 +0100

    3,2,1 countdown (as posted to stackoverflow)

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -12,14 +12,57 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.threeTwoOneCounter()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    func threeTwoOneCounter() {
+        let fadeInTime:Double = 1.0
+        let fadeOutTime:Double = 0.5
+        let label = UILabel(frame: CGRectMake(self.view.bounds.width/2-50,100,100,100))
+        label.text = "3"
+        label.font = UIFont(name: "Arial", size: 50)
+        label.textColor = UIColor.blackColor()
+        label.textAlignment=NSTextAlignment.Center
+        label.alpha = 0.0
+        self.view.addSubview(label)
 
+        UIView.animateWithDuration(fadeInTime, animations: { () -> Void in
+            label.alpha = 1.0
+        }) { (Bool) -> Void in
+            
+            UIView.animateWithDuration(fadeOutTime, animations: { () -> Void in
+                label.alpha = 0.0
+            }, completion: { (Bool) -> Void in
+                label.text = "2"
+                UIView.animateWithDuration(fadeInTime, animations: { () -> Void in
+                    label.alpha = 1.0
+                }, completion: { (Bool) -> Void in
+                    UIView.animateWithDuration(fadeOutTime, animations: { () -> Void in
+                        label.alpha = 0.0
+                    }, completion: { (Bool) -> Void in
+                        label.text = "1"
+                        UIView.animateWithDuration(fadeInTime, animations: { () -> Void in
+                            label.alpha=1.0
+                        }, completion: { (BOOl) -> Void in
+                            UIView.animateWithDuration(fadeOutTime, animations: { () -> Void in
+                                label.alpha = 0.0
+                            }, completion: { (Bool) -> Void in
+                                println("done.maybe calling delegate now")
+                            })
+                        })
+                    })
+                })
+                
+            })
+        }
+        
+        
+    
+        
+        
+    }
+    
+    
 
 }
 
